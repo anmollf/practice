24c53cf2d3039102c9fd78978a8d8aa5824aab31
Anmol Madaik <anmol.m@idsil.loc>
2024-01-22 18:13:36 +0530

    removed xtra folders

--- myproject/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    product_id = models.CharField(max_length = 8)
-    name = models.CharField(max_length = 30)
-    issue = models.DateField()
